1a1ee5ab54e64fccc22ffa48a72d3c8f141900e7
Efflam Castel <efflamm.castel@gmail.com>
2022-06-24 00:11:07 +0000

    chessboard and bootstrap

--- Gemfile
+++ Gemfile
@@ -6,6 +6,8 @@ ruby "3.0.1"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.3"
 gem 'mongoid'
+gem 'sassc-rails'
+gem 'bootstrap'
 
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,5 @@
 
 pin "application", preload: true
 pin "chessground", to: "https://ga.jspm.io/npm:chessground@8.2.1/chessground.js"
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"
