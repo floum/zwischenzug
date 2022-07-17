71b43d9e0b2f34c2d426f92a8046a0beefbeeea3
Efflam Castel <efflamm.castel@gmail.com>
2022-07-17 00:59:41 +0000

    showing games

--- app/javascript/chessgame.js
+++ app/javascript/chessgame.js
@@ -4,17 +4,20 @@ import { Chess } from 'chess.js';
 export class Chessgame {
   constructor(element, pgn) {
     this.element = element;
-    this.game = new Chess();
-    this.game.load_pgn(pgn);
-    this.board = undefined;
-    this.boardConfig = {
-      fen: this.game.fen()
+    this._game = new Chess();
+    this._game.load_pgn(pgn);
+    this.moves = this._game.history({verbose: true});
+    this.fens = [];
+    this._game = new Chess();
+    this.fens.push(this._game.fen());
+    for (var i in this.moves) {
+      this._game.move(this.moves[i]);
+      this.fens.push(this._game.fen());
     };
+    this.board = Chessground(this.element, {});
+    this._plyNumber = 0;
   }
 
-  render() {
-    this.board = Chessground(this.element, this.boardConfig);
-  }
 
   setFen(fen) {
     this.board.set({ fen: fen });
@@ -23,4 +26,31 @@ export class Chessgame {
   setOrientation(color) {
     this.board.set({ orientation: color });
   }
+
+  startMove() {
+    this.plyNumber = 0;
+  }
+
+  forwardMove() {
+    this.plyNumber = this.plyNumber + 1;
+  }
+
+  backMove() {
+    this.plyNumber -= 1;
+  }
+
+  endMove() {
+    this.plyNumber = this.fens.length - 1;
+  }
+
+  get plyNumber() {
+    return this._plyNumber;
+  }
+
+  set plyNumber(number) {
+    this._plyNumber = number;
+    this._plyNumber = Math.max(0, this._plyNumber);
+    this._plyNumber = Math.min(this._plyNumber, this.fens.length - 1);
+    this.board.set({ fen: this.fens[this._plyNumber]});
+  };
 }
