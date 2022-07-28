4a70caf92041f3af0765177d39dc6b072d2f9f5c
Efflam Castel <efflamm.castel@gmail.com>
2022-07-28 14:40:40 +0200

    api namespace & improving routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,19 @@ Rails.application.routes.draw do
   delete 'session', to: 'session#destroy'
   resources :users, only: [:new, :create, :update]
 
-  resources :games, only: [:index, :new, :create, :destroy, :show]
+  resources :games, only: [:index, :new, :create, :destroy, :show] do
+    post :import, on: :collection
+  end
+
   resources :game_analysis
 
-  post 'games/import', to: 'games#import'
-
   root 'games#index'
+
+  namespace 'api', defaults: {format: :json} do
+    resources :games do
+      post :import, on: :collection
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
