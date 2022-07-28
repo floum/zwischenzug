ce1b5b42f844c31289a2110b3b9122ffc93edf11
Efflamm Castel <efflamm.castel@gmail.com>
2022-07-28 18:22:10 +0200

    progression & icon

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :game_analysis
 
-  root 'games#index'
+  root 'game_analysis#index'
 
   namespace 'api', defaults: {format: :json} do
     resources :games do
