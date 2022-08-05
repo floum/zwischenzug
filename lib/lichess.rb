30c4a7fbc624f7fef823ca6cdd7dfa456bfe7962
Efflam Castel <efflamm.castel@gmail.com>
2022-08-05 15:50:14 +0200

    import lichess ID with full url

--- lib/lichess.rb
+++ lib/lichess.rb
@@ -1,5 +1,7 @@
 class Lichess
   def self.fetch(id)
-    Net::HTTP.get(URI.parse("https://lichess.org/game/export/#{id}?evals=false&clocks=false")).force_encoding('UTF-8')
+    id = id.split('/').last[0..7]
+    response = Net::HTTP.get(URI.parse("https://lichess.org/game/export/#{id}?evals=false&clocks=false")).force_encoding('UTF-8')
+    response
   end
 end
